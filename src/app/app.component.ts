188a8a7d4c8e6e1920b048dd8ea7a723602bdff2
Massimiliano <masdelmon@hotmail.it>
2019-02-15 00:01:15 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 
-@Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+@Component ({
+   selector: 'app-root',
+   template: `
+   <ul>
+      <li><a [routerLink] = "['/personal']">Personal</a></li>
+      <li><a [routerLink] = "['/Experiences']">Experiences</a></li>
+   </ul>
+   <router-outlet></router-outlet>`
 })
 export class AppComponent {
-  title = "CodeSandbox";
+  title = "myCV";
 }
+
+
+
+
