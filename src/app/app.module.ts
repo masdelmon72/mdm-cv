e6dcdd5a41ec91d5e7ef3ce6e10abfbda7871033
Massimiliano <masdelmon@hotmail.it>
2019-02-14 23:15:12 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppComponent } from "./app.component";
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+import { Apppersonal } from './personal.component' 
+import { AppExperiences } from  './experiences.component'
+
+import { PageNotFoundComponent } from  './NotFound.component' 
+import { RouterModule, Routes } from '@angular/router';  
+
+const appRoutes: Routes = [ 
+   { path: 'Personal', component: Apppersonal }, 
+   { path: 'Experiences', component: AppExperiences }, 
+   { path: '**', component: PageNotFoundComponent } 
+];  
+
+@NgModule ({ 
+   imports: [ BrowserModule, 
+   RouterModule.forRoot(appRoutes)], 
+   declarations: [ AppComponent,Apppersonal,AppExperiences,PageNotFoundComponent], 
+   bootstrap: [ AppComponent ] 
+}) 
+
+export class AppModule {
+} 
