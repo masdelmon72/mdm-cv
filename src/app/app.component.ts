f1e2603d674f9cd5e4ade5dc5e55d8f11384d2ec
Massimiliano <masdelmon@hotmail.it>
2019-02-15 01:47:03 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component } from "@angular/core";
-
-@Component ({
-   selector: 'app-root',
-   template: `
-   <ul>
-      <li><a [routerLink] = "['/personal']">Personal</a></li>
-      <li><a [routerLink] = "['/Experiences']">Experiences</a></li>
-   </ul>
-   <router-outlet></router-outlet>`
-})
-export class AppComponent {
-  title = "myCV";
-}
-
+<h1>{{title}}</h1>
+<nav>
+  <a routerLink="/personal">Personal Data</a>
+  <a routerLink="/experiences">Work experience</a>
+</nav>
+<router-outlet></router-outlet>
+<app-messages></app-messages>
 
 
 
