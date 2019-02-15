411160170477ff3d49324f91ee1ee57a705e1e48
Massimiliano <masdelmon@hotmail.it>
2019-02-15 01:59:41 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent }         from './app.component';
-import { PersonalComponent }   from './app.personal.component';
-import { ExperiencesComponent }  from './app.experiences.component';
+import { PersonalComponent }   from './personal.component';
+import { ExperiencesComponent }  from './experiences.component';
 
-import { AppModule }     from './app-routing.module';
+import { AppRouteModule }     from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppModule
+    AppRouteModule
   ],
   declarations: [
     AppComponent,
