22980f09d47f6b9ea64c96cc388f5e2df8b15a9c
Massimiliano <masdelmon@hotmail.it>
2019-02-15 02:00:33 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 <h1>{{title}}</h1>
 <nav>
-  <a routerLink="/personal">Personal Data</a>
-  <a routerLink="/experiences">Work experience</a>
+  <a routerLink="/Personal">Personal Data</a>
+  <a routerLink="/Experiences">Work experience</a>
 </nav>
 <router-outlet></router-outlet>
 <app-messages></app-messages>
