36e132fcb2d66700dadecc0b74293381f1e8a1aa
Massimiliano <masdelmon@hotmail.it>
2019-02-15 16:57:57 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { NgModule }      from '@angular/core';
+import { RouterModule}   from '@angular/router';
+
+import { AppComponent }    from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @NgModule({
    declarations: [
-      AppComponent
+      AppComponent,
+      NewCmpComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      RouterModule.forRoot([
+         {
+            path: 'new-cmp',
+            component: NewCmpComponent
+         }
+      ])
    ],
    providers: [],
    bootstrap: [AppComponent]
